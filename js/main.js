f737dea2daf5400277193e89bde4f436155f8e56
mikhail <rabota822@bk.ru>
2025-02-08 02:21:27 +0300

    refactor js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 function validatePhone(){
     let phone = document.querySelector('#tel').value;
+    let nameInput = document.querySelector('#name');
+    let error = document.querySelector('.error');
     var pattern = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
-     if(pattern.test(phone)){
-
-        
-        console.log(phone + ' norma');
+     if(pattern.test(phone)){        
+        console.log(phone);       
      } else{
-        console.log('ne norma');
-     }
-    
+        error.style.display = 'inline';
+        error.style.color = 'red';
+        nameInput.style.marginTop = '15px';
+        console.log('Number not validate!!');
+     }    
 }
+
 
